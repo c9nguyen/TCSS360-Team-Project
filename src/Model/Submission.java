b0def3c6bfcdbc8afeeafd73f703c2e807f21f33
ajasif2 <ajasif@gmail.com>
2016-05-24 16:26:45 -0700

    Added constructor.

--- src/Model/Submission.java
+++ src/Model/Submission.java
@@ -1,7 +1,12 @@
+// Test
+// Asif Jamal
+// This class contains information about a user submission.
 package Model;
 
 public class Submission {
 	private String mySubmissionName;
 	
-	
+	public Submission() {
+		
+	}
 }
